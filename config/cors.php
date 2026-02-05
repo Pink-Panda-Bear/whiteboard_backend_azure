17f36113bf2a03a07ff36304fd99c5a01f8ea748
Ivan <pijak.ivan@gmail.com>
2026-02-05 21:56:15 +0100

    Initial commit

--- config/cors.php
+++ config/cors.php
@@ -19,10 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'http://localhost:5173',
-        'https://whiteboard1-fze2dkfwvgecfc.azurewebsites.net',
-    ],
+    'allowed_origins' => ['http://localhost:5173'],
 
     'allowed_origins_patterns' => [],
 
